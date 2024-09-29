0c74a059829d41323d5d896fd22722dd25a38646
JoacoDv <joacodv2003@gmail.com>
2024-09-29 20:51:02 -0300

    Arreglar .style.display problem

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const resultPlay = document.querySelector(".result-play");
 const buttonPiedra = document.querySelector(".btn-piedra");
 const buttonPapel = document.querySelector(".btn-papel");
 const buttonTijera = document.querySelector(".btn-tijera");
+const buttonReset = document.querySelector(".btn-reset");
+const controls = document.querySelector("controls")
 const textWinner = document.querySelector(".winner");
 const winner = document.querySelector(".winner");
 const arrChoise = ["piedra", "papel", "tijera"];
 let scorePc = 0;
 let scorePlayer = 0;
+
+function pcPlay() {
+    const playing = arrChoise[Math.floor(Math.random() * 3)];
+    return playing;
+}
 
 function play(playing, pcPlaying) {
     if (playing === pcPlaying){
@@ -17,25 +24,39 @@ function play(playing, pcPlaying) {
         playing === "papel" && pcPlaying === "piedra" ||
         playing === "tijera" && pcPlaying === "papel"
     ){
-        resultPlay.innerText = `Ganaste, elegiste ${playing} y la Pc eligio ${pcPlaying}.`;
+        resultPlay.innerText = `Ganaste esta ronda, elegiste ${playing} y la Pc eligio ${pcPlaying}.`;
         scorePlayer++;
         scorePlayerText.innerText = scorePlayer;
-        if (scorePlayer = 3){
-            winner.innerText = "Ganaste";
+        if (scorePlayer === 3){
+            textWinner.innerText = "Ganaste ¿Quieres jugar de nuevo?";
+            controls.style.display = 'none';
+            buttonReset.style.display = "none";
         }
     } else {
-        resultPlay.innerText = `Perdiste, elegiste ${playing} y la Pc eligio ${pcPlaying}`;
+        resultPlay.innerText = `Perdiste esta ronda, elegiste ${playing} y la Pc eligio ${pcPlaying}`;
         scorePc++;
         scorePcText.innerText = scorePc;
-        if (scorePc = 3){   
-            winner.innerText = "Perdiste";
+        if (scorePc === 3){   
+            textWinner.innerText = "Perdiste ¿Quieres jugar de nuevo?";
+            controls.style.display = "none";
+            buttonReset.style.display = "none";
         }
     }
 }
 
-function pcPlay() {
-    const playing = arrChoise[Math.floor(Math.random() * 3)];
-    return playing;
+function reset() {
+    scorePc = 0;
+    scorePlayer = 0;
+    scorePcText.innerText = scorePc;
+    scorePlayerText.innerText = scorePlayer;
+    resultPlay.innerTeext = "";
+    textWinner.innerText = "";
+    controls.style.display = "inline";
+    buttonReset.style.display = "block";
 }
 
+buttonPiedra.addEventListener("click", function () { play("piedra", pcPlay())});
+buttonPapel.addEventListener("click", function () { play("papel", pcPlay())});
+buttonTijera.addEventListener("click", function () { play("tijera", pcPlay())});
+buttonReset.addEventListener("click", function () {reset()})
 
